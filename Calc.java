3ab617a806d3e85bc39e08b1bfbc290777a1d3d0
shweta-thakur19 <72908150+shweta-thakur19@users.noreply.github.com>
2020-10-20 11:18:11 +0530

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -7,9 +7,14 @@ class Calc{
    public static int sub(int a,int b){
     return a-b;
    }
+   
+   public static int prod(int a,int b){
+    return a*b;
+   }
 
    public static void main(String []args){
        System.out.println(add(10,20));
       System.out.println(sub(40,20));
+      System.out.println(prod(4,2));
    }
 }
